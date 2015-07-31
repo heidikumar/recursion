259b998c388d1d6ef8f684e9f33f76ef45a938d6
Heidi Kumar <heidi.kristine.hansen@gmail.com>
2015-07-31 08:48:18 -0700

    Added and fixed stringifyJSON function.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,54 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+
+  if(obj===null){
+    return "null";
+  }
+  if(obj===undefined){
+    return;
+  }
+  if (typeof obj == "function"){
+    return;
+  }
+  if(typeof obj ==="boolean" || typeof obj === "number"){
+    var ans = "" + obj + "";
+    return ans;
+  }
+  if (typeof obj ==="string"){
+    var ans = "\"" + obj + "\""
+    return ans;
+  }
+  if (Array.isArray(obj)){
+    var ans = [];
+    if (obj.length === 0){
+      ans = "[]";
+    } else {
+      for (var i=0; i<obj.length; i++){
+         ans.push(stringifyJSON(obj[i]));
+      }
+      ans = "[" + ans.join(",") + "]";
+    }
+    return ans;
+  } 
+  if(Object.getOwnPropertyNames(obj).length === 0){
+    var ans = "{"
+  } else {
+    var ans = "{"
+      for (var key in obj){       
+        var saveKey = stringifyJSON(key);
+        var saveVal = stringifyJSON(obj[key]);
+        delete obj[key];
+        if(saveVal){
+          var temp = saveKey + ":" +saveVal;
+            if(Object.getOwnPropertyNames(obj).length>0){
+            temp += ",";
+            }
+          ans += temp;
+        }
+      }
+  }
+  ans += "}";
+  return ans;
+}
+
